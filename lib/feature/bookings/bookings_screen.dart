96531bd2e5744c0bb903df02bdadda20cc5bc80c
Amanjot Singh <amanjot.s@triveous.com>
2024-03-20 19:30:10 +0530

    Theme: Completed Textstyles, Buttons, Colors.

--- lib/feature/bookings/bookings_screen.dart
+++ lib/feature/bookings/bookings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:silver_genie/core/constants/text_styles.dart';
 import 'package:silver_genie/core/widgets/app_bar.dart';
 
 class BookingsScreen extends StatelessWidget {
@@ -9,7 +10,10 @@ class BookingsScreen extends StatelessWidget {
     return const Scaffold(
       appBar: Appbar(),
       body: Center(
-        child: Text('Bookings Screen'),
+        child: Text(
+          'Bookings Screen',
+          style: AppTextStyle.heading3Bold,
+        ),
       ),
     );
   }
--- lib/feature/services/services_screen.dart
+++ lib/feature/services/services_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:silver_genie/core/constants/text_styles.dart';
 import 'package:silver_genie/core/widgets/app_bar.dart';
 
 class ServicesScreen extends StatelessWidget {
@@ -9,7 +10,10 @@ class ServicesScreen extends StatelessWidget {
     return const Scaffold(
       appBar: Appbar(),
       body: Center(
-        child: Text('Services Screen'),
+        child: Text(
+          'Services Screen',
+          style: AppTextStyle.heading3Bold,
+        ),
       ),
     );
   }
