8f882555f6423475f2ce95d211153819abd81600
kiran isac <kiran.i@triveous.com>
2024-03-22 12:51:16 +0530

    Added dimensions

---
+++ lib/core/constants/dimensions.dart
@@ -0,0 +1,28 @@
+abstract class Dimension {
+  Dimension._();
+
+  static double arbitary(double value) => value;
+
+  static const _scaleFactor = 4.0;
+  static const d0 = 0.0;
+  static const d1 = _scaleFactor * 1;
+  static const d2 = _scaleFactor * 2;
+  static const d3 = _scaleFactor * 3;
+  static const d4 = _scaleFactor * 4;
+  static const d5 = _scaleFactor * 5;
+  static const d6 = _scaleFactor * 6;
+  static const d7 = _scaleFactor * 7;
+  static const d8 = _scaleFactor * 8;
+  static const d9 = _scaleFactor * 9;
+  static const d10 = _scaleFactor * 10;
+  static const d11 = _scaleFactor * 11;
+  static const d12 = _scaleFactor * 12;
+  static const d13 = _scaleFactor * 13;
+  static const d14 = _scaleFactor * 14;
+  static const d15 = _scaleFactor * 15;
+  static const d16 = _scaleFactor * 16;
+  static const d17 = _scaleFactor * 17;
+  static const d18 = _scaleFactor * 18;
+  static const d19 = _scaleFactor * 19;
+  static const d20 = _scaleFactor * 20;
+}
