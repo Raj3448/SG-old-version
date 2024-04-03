6902c6a7cde0f94f0c7ac14ddf92c7eb1c3f3071
yashaswi23 <75798032+yashaswi23@users.noreply.github.com>
2024-04-03 11:54:08 +0530

    conflicts resolved

--- lib/core/routes/routes_constants.dart
+++ lib/core/routes/routes_constants.dart
@@ -12,7 +12,7 @@ class RoutesConstants {
   static const serviceRoute = '/service';
   static const bookingsRoute = '/bookings';
   static const membersRoute = '/members';
-  static const userProfileRoute='/user-profile';
+  static const userProfileScreen='/user-profile';
 
   //---- Screens ------------------//
   static const addEditFamilyMemberRoute = '/addEditFamilyMember';
