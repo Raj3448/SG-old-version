c75d55ddf530d30097fa92a0958261143f31de17
Amanjot Singh <amanjot.s@triveous.com>
2024-04-25 15:07:47 +0530

    Update: Added constructor injection, replace error with Failure

--- lib/feature/onboarding/service/master_service.dart
+++ lib/feature/onboarding/service/master_service.dart
@@ -1,20 +1,23 @@
 // ignore_for_file: inference_failure_on_function_invocation, one_member_abstracts
 
 import 'package:fpdart/fpdart.dart';
-import 'package:get_it/get_it.dart';
+import 'package:silver_genie/core/failure/failure.dart';
 import 'package:silver_genie/core/utils/http_client.dart';
 import 'package:silver_genie/feature/onboarding/models/master_data_model.dart';
 
 abstract class IMasterService {
-  Future<Either<Error, MasterDataModel>> getAllMasterData();
+  Future<Either<Failure, MasterDataModel>> getAllMasterData(
+    HttpClient httpClient,
+  );
 }
 
 class MasterDataService implements IMasterService {
-  final dio = GetIt.I<HttpClient>();
-
   @override
-  Future<Either<Error, MasterDataModel>> getAllMasterData() async {
+  Future<Either<Failure, MasterDataModel>> getAllMasterData(
+    HttpClient httpClient,
+  ) async {
     try {
+      final dio = httpClient;
       final response =
           await dio.get('https://silvergenie.com/api/v1/masterData');
 
@@ -23,21 +26,10 @@ class MasterDataService implements IMasterService {
         final masterData = MasterDataModel.fromJson(jsonData);
         return Right(masterData);
       } else {
-        return Left(
-          Error(
-            'Failed to load master data',
-          ),
-        );
+        return const Left(Failure.badResponse());
       }
     } catch (e) {
-      return Left(
-        Error('An error occurred: $e'),
-      );
+      return const Left(Failure.someThingWentWrong());
     }
   }
-}
-
-class Error {
-  Error(this.message);
-  final String message;
 }
