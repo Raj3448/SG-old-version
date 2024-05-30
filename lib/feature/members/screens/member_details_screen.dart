07ae93cb76f9d01c1a5dab9942139d33ed6f2d96
Amanjot Singh <163853225+amanjot32@users.noreply.github.com>
2024-05-30 13:10:02 +0530

    Fix: Address field, PHR

--- lib/feature/members/screens/member_details_screen.dart
+++ lib/feature/members/screens/member_details_screen.dart
@@ -107,7 +107,7 @@ class MemberDetailsScreen extends StatelessWidget {
                     children: [
                       HealthCard(
                         isEpr: true,
-                        dateUpdated: '25/03/2024',
+                        dateUpdated: formatDateTime(activeMember!.updatedAt),
                         ontap: () {
                           GoRouter.of(context).pushNamed(
                             RoutesConstants.eprRoute,
@@ -118,20 +118,22 @@ class MemberDetailsScreen extends StatelessWidget {
                         },
                       ),
                       const SizedBox(height: 16),
-                      HealthCard(
-                        isEpr: false,
-                        dateUpdated: '25/03/2024',
-                        ontap: activeMember!.phrModel == null
-                            ? null
-                            : () {
-                                GoRouter.of(context).pushNamed(
+                      if (activeMember!.phrModel != null)
+                        HealthCard(
+                          isEpr: false,
+                          dateUpdated: formatDateTime(activeMember!.updatedAt),
+                          ontap: activeMember!.phrModel == null
+                              ? null
+                              : () {
+                                  GoRouter.of(context).pushNamed(
                                     RoutesConstants.phrPdfViewPage,
                                     pathParameters: {
                                       'memberPhrId':
-                                          activeMember!.phrModel!.id.toString()
-                                    });
-                              },
-                      ),
+                                          activeMember!.phrModel!.id.toString(),
+                                    },
+                                  );
+                                },
+                        ),
                       const SizedBox(height: Dimension.d17),
                     ],
                   )
@@ -180,15 +182,15 @@ class _BasicDetailsBox extends StatelessWidget {
     required this.age,
     required this.gender,
     required this.relation,
-    required this.mobileNo,
     required this.imgPath,
+    this.mobileNo,
     this.address,
   });
   final String name;
   final String age;
   final String gender;
   final String? relation;
-  final String mobileNo;
+  final String? mobileNo;
   final String? address;
   final String imgPath;
 
@@ -246,44 +248,54 @@ class _BasicDetailsBox extends StatelessWidget {
             ],
           ),
           const SizedBox(height: Dimension.d3),
-          Row(
-            children: [
-              const Icon(
-                AppIcons.phone,
-                color: AppColors.grayscale700,
-                size: 17,
-              ),
-              const SizedBox(width: Dimension.d3),
-              Text(
-                mobileNo,
-                style: AppTextStyle.bodyLargeMedium
-                    .copyWith(color: AppColors.grayscale900),
-              ),
-            ],
-          ),
-          const SizedBox(height: Dimension.d3),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Icon(
-                  AppIcons.home,
+          if (mobileNo!.isEmpty)
+            const SizedBox()
+          else
+            Row(
+              children: [
+                const Icon(
+                  AppIcons.phone,
                   color: AppColors.grayscale700,
                   size: 17,
                 ),
-              ),
-              const SizedBox(width: Dimension.d3),
-              SizedBox(
-                width: 300,
-                child: Text(
-                  address!,
+                const SizedBox(width: Dimension.d3),
+                Text(
+                  mobileNo!,
                   style: AppTextStyle.bodyLargeMedium
                       .copyWith(color: AppColors.grayscale900),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          if (address!.isEmpty)
+            const SizedBox()
+          else
+            Column(
+              children: [
+                const SizedBox(height: Dimension.d3),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Icon(
+                        AppIcons.home,
+                        color: AppColors.grayscale700,
+                        size: 17,
+                      ),
+                    ),
+                    const SizedBox(width: Dimension.d3),
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        address!,
+                        style: AppTextStyle.bodyLargeMedium
+                            .copyWith(color: AppColors.grayscale900),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           const SizedBox(height: Dimension.d3),
           CustomButton(
             ontap: () {
@@ -306,4 +318,8 @@ class _BasicDetailsBox extends StatelessWidget {
       ),
     );
   }
+}
+
+String formatDateTime(DateTime dateTime) {
+  return DateFormat('MM/dd/yyyy').format(dateTime);
 }
