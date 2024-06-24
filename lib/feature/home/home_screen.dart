21d69c64eed9544ef4eca2259b3807332f420237
Amanjot Singh <163853225+amanjot32@users.noreply.github.com>
2024-06-24 14:14:13 +0530

    Add: Call dialer trigger

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -625,6 +625,20 @@ class _EmergencyActivateBottomSheet extends StatefulWidget {
 class _EmergencyActivateBottomSheetState
     extends State<_EmergencyActivateBottomSheet> {
   bool isActivate = false;
+
+  void launchDialer(String phoneNumber) async {
+    final telUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+
+    if (await canLaunchUrl(telUri)) {
+      await launchUrl(telUri);
+    } else {
+      throw Exception('Could not launch $telUri');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -659,7 +673,7 @@ class _EmergencyActivateBottomSheetState
                         ),
                       ),
                       onPressed: () {
-                        Navigator.of(context).pop();
+                        context.pop();
                       },
                     ),
                   ],
@@ -675,6 +689,7 @@ class _EmergencyActivateBottomSheetState
                 ),
                 _ActivateListileComponent(
                   onPressed: () {
+                    launchDialer('+910000000000');
                     setState(() {
                       isActivate = true;
                     });
@@ -682,6 +697,7 @@ class _EmergencyActivateBottomSheetState
                 ),
                 _ActivateListileComponent(
                   onPressed: () {
+                    launchDialer('+910000000000');
                     setState(() {
                       isActivate = true;
                     });
