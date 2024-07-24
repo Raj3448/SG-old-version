a64bc6735be316c59dce4498fc9915d5f23c20fc
Raj344894 <rajkumar.c@triveous.com>
2024-07-24 16:12:27 +0530

    member refresh

--- lib/feature/members/screens/family_screen.dart
+++ lib/feature/members/screens/family_screen.dart
@@ -18,24 +18,14 @@ import 'package:silver_genie/feature/members/store/members_store.dart';
 import 'package:silver_genie/feature/members/widgets/member_card.dart';
 import 'package:silver_genie/feature/user_profile/store/user_details_store.dart';
 
-class FamilyScreen extends StatefulWidget {
+class FamilyScreen extends StatelessWidget {
   const FamilyScreen({super.key});
 
   @override
-  State<FamilyScreen> createState() => _FamilyScreenState();
-}
-
-class _FamilyScreenState extends State<FamilyScreen> {
-  late MembersStore store;
-  @override
-  void initState() {
-    store = GetIt.I<MembersStore>()..init();
-    store.refresh();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final store = GetIt.I<MembersStore>()..init();
+    // ignore: cascade_invocations
+    store.refresh();
     return Scaffold(
       backgroundColor: AppColors.white,
       body: Padding(
