b76f46b0ed172b938766950edd039f739fe6c4f9
Raj344894 <rajkumar.c@triveous.com>
2024-07-29 18:12:31 +0530

    fixes

--- lib/feature/book_services/screens/book_service_screen.dart
+++ lib/feature/book_services/screens/book_service_screen.dart
@@ -174,18 +174,7 @@ class _BookServiceScreenState extends State<BookServiceScreen>
                               memberList: GetIt.I<MembersStore>().familyMembers,
                               placeHolder: component.formDetails.placeholder,
                               updateMember: (member) {
-                                if (member != null &&
-                                    member.subscriptions != null &&
-                                    member.subscriptions!.isEmpty) {
-                                  setState(() {
-                                    selectedMember = null;
-                                  });
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) => _BuyPlanComponent(),
-                                  );
-                                } else {
-                                  selectedMember = member;
+                                selectedMember = member;
                                   _updateFormValues1(
                                     id: component.id,
                                     title: component.formDetails.title,
@@ -197,7 +186,30 @@ class _BookServiceScreenState extends State<BookServiceScreen>
                                     ],
                                     forDId: component.formDetails.id.toString(),
                                   );
-                                }
+                                // if (member != null &&
+                                //     member.subscriptions != null &&
+                                //     member.subscriptions!.isEmpty) {
+                                //   setState(() {
+                                //     selectedMember = null;
+                                //   });
+                                //   showDialog(
+                                //     context: context,
+                                //     builder: (context) => _BuyPlanComponent(),
+                                //   );
+                                // } else {
+                                //   selectedMember = member;
+                                //   _updateFormValues1(
+                                //     id: component.id,
+                                //     title: component.formDetails.title,
+                                //     type: component.type,
+                                //     controlType: component.controlType,
+                                //     hint: component.formDetails.hint,
+                                //     valueReference: [
+                                //       '${member?.id}',
+                                //     ],
+                                //     forDId: component.formDetails.id.toString(),
+                                //   );
+                                // }
                               },
                               isRequired: component.formDetails.required,
                             ),
@@ -570,46 +582,46 @@ class _BookServiceScreenState extends State<BookServiceScreen>
   }
 }
 
-class _BuyPlanComponent extends StatelessWidget {
-  final store = GetIt.I<ProductListingStore>();
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      insetPadding: const EdgeInsets.symmetric(horizontal: Dimension.d4),
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          borderRadius: BorderRadius.circular(Dimension.d2),
-        ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: Dimension.d4,
-          vertical: Dimension.d6,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Hi there!!',
-              style: AppTextStyle.bodyXLSemiBold
-                  .copyWith(color: AppColors.grayscale900, height: 2.4),
-            ),
-            Text(
-              'The selected member is not subscriber of any plan. Upgrade to any plan to access our services.',
-              textAlign: TextAlign.center,
-              style: AppTextStyle.bodyLargeMedium
-                  .copyWith(color: AppColors.grayscale900),
-            ),
-            const SizedBox(height: Dimension.d2),
-            if (store.getSubscriptActiveProdList.isNotEmpty)
-              ProductListingCareComponent(
-                productBasicDetailsList: store.getProdListRankOrder(
-                  store.getSubscriptActiveProdList,
-                ),
-                isUpgradeable: false,
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class _BuyPlanComponent extends StatelessWidget {
+//   final store = GetIt.I<ProductListingStore>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Dialog(
+//       insetPadding: const EdgeInsets.symmetric(horizontal: Dimension.d4),
+//       child: Container(
+//         decoration: BoxDecoration(
+//           color: AppColors.white,
+//           borderRadius: BorderRadius.circular(Dimension.d2),
+//         ),
+//         padding: const EdgeInsets.symmetric(
+//           horizontal: Dimension.d4,
+//           vertical: Dimension.d6,
+//         ),
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             Text(
+//               'Hi there!!',
+//               style: AppTextStyle.bodyXLSemiBold
+//                   .copyWith(color: AppColors.grayscale900, height: 2.4),
+//             ),
+//             Text(
+//               'The selected member is not subscriber of any plan. Upgrade to any plan to access our services.',
+//               textAlign: TextAlign.center,
+//               style: AppTextStyle.bodyLargeMedium
+//                   .copyWith(color: AppColors.grayscale900),
+//             ),
+//             const SizedBox(height: Dimension.d2),
+//             if (store.getSubscriptActiveProdList.isNotEmpty)
+//               ProductListingCareComponent(
+//                 productBasicDetailsList: store.getProdListRankOrder(
+//                   store.getSubscriptActiveProdList,
+//                 ),
+//                 isUpgradeable: false,
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
